ce9631ec47970cb2c0fddff6c6ad05a4aee9dc3c
Felix Orinda <forinda82@gmail.com>
2024-11-10 23:13:49 +0300

    Add pagination options and make context body generic

--- src/common/interfaces/controller.ts
+++ src/common/interfaces/controller.ts
@@ -1,17 +1,19 @@
 import type { NextFunction } from 'express';
 import type { ApiReq, ApiRes } from '../http';
+import type { ApiPaginationParams } from '../utils/pagination';
 
 type ContextParams = Pick<ApiReq, 'params'>['params'];
 type ContextQuery = Pick<ApiReq, 'query'>['query'];
-type ContextBody = Pick<ApiReq, 'body'>['body'];
+// type ContextBody = Pick<ApiReq, 'body'>['body'];
 
-export type ApiRequestContext = {
+export type ApiRequestContext<Body = any> = {
   req: ApiReq;
   res: ApiRes;
   next: NextFunction;
   params?: ContextParams;
   query?: ContextQuery;
-  body?: ContextBody;
+  body?: Body;
+  pagination?: ApiPaginationParams;
 };
 
 // Define a generic ControllerMethod type
