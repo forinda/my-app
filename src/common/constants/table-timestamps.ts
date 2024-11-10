fececa2f4d790c3498b3de09b0e4622245b67ebf
Felix Orinda <forinda82@gmail.com>
2024-11-10 23:13:05 +0300

    Fix table updated timestamp

--- src/common/constants/table-timestamps.ts
+++ src/common/constants/table-timestamps.ts
@@ -1,10 +1,16 @@
+import { sql } from 'drizzle-orm';
 import { timestamp } from 'drizzle-orm/pg-core';
 
 const createdTimestamps = timestamp('created_at', { mode: 'string' })
   .notNull()
-  .default('now()');
+  .defaultNow();
 
-const updatedTimestamps = timestamp('updated_at', { mode: 'string' }).notNull();
+const updatedTimestamps = timestamp('updated_at', {
+  mode: 'string'
+})
+  .defaultNow()
+  .$onUpdate(() => sql`CURRENT_TIMESTAMP`)
+  .notNull();
 
 const deletedTimestamps = timestamp('deleted_at', { mode: 'string' });
 
