d2ec8b77eb1f38c373106bdca77dcecc1af3afb9
Felix Orinda <forinda82@gmail.com>
2024-11-10 23:11:21 +0300

    Add table relations

--- src/db/schema/user-table.ts
+++ src/db/schema/user-table.ts
@@ -1,11 +1,15 @@
 import { getTableTimestamps } from '@/common/constants/table-timestamps';
+import { relations } from 'drizzle-orm';
 import { boolean, date, integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { Token } from './token-table';
+import { LoginSession } from './login-session';
 
-export const UserTable = pgTable('users', {
-  id: integer().primaryKey(),
+export const User = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
   first_name: varchar().notNull(),
   last_name: varchar().notNull(),
   email: varchar().notNull().unique(),
+  gender: varchar().notNull(),
   username: varchar().notNull().unique(),
   password: varchar().notNull(),
   is_active: boolean().notNull().default(false),
@@ -13,5 +17,11 @@ export const UserTable = pgTable('users', {
   last_login_at: date({ mode: 'string' }),
   last_login_ip: varchar(),
   needs_to_reset_password: boolean().notNull().default(false),
+  last_password_reset_at: date({ mode: 'string' }),
   ...getTableTimestamps()
 });
+
+export const userRelations = relations(User, ({ many }) => ({
+  tokens: many(Token),
+  login_sessions: many(LoginSession)
+}));
