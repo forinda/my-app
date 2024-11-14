04aa11390a4255d0c5d4eb667a70c1dcb64ee3db
Felix Orinda <forinda82@gmail.com>
2024-11-14 12:40:59 +0300

    Day-7 Add test setup with vitest

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'url';
+export default defineConfig({
+  test: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    environment: 'node',
+    setupFiles: ['./src/test/setup.ts']
+    // include: ['src/**/*.{.spec,.test}.ts']
+  }
+});
