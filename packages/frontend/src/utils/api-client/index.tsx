38b8ce1d25454042e01c07df76c7cbb547053161
Felix Orinda <forinda82@gmail.com>
2025-01-13 00:43:43 +0300

    Change defaul;t port to avoid port collision with adminer

--- packages/frontend/src/utils/api-client/index.tsx
+++ packages/frontend/src/utils/api-client/index.tsx
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: 'http://localhost:5050/api/v1',
 });
 
 export default apiClient;
