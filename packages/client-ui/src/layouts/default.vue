deda6d4c8fc9cca8a7ef86e38cafca7447a0d7b2
Felix Orinda <forinda82@gmail.com>
2025-02-13 01:44:45 +0300

    Add layouts

---
+++ packages/client-ui/src/layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <nav-bar></nav-bar>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ packages/client-ui/src/layouts/org.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto p-6">
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
