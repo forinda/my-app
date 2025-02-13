6d725cc7116e18bf5eb5d37daf90fe46fac6e40e
Felix Orinda <forinda82@gmail.com>
2025-02-13 08:11:13 +0300

    Fix build for frontend

--- packages/frontend/src/components/home-banner.tsx
+++ packages/frontend/src/components/home-banner.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import MaxWidth from './max-width';
 
 export default function HomeBanner() {
@@ -10,8 +9,12 @@ export default function HomeBanner() {
           <span className="text-primary font-bold">easy-use Payroll,</span>
           tracking software
         </h1>
-        <div className='w-full'>
-          <img src="/logo-003.svg" alt="home-banner" className="py-4 w-full max-h-80 h-full" />
+        <div className="w-full">
+          <img
+            src="/logo-003.svg"
+            alt="home-banner"
+            className="py-4 w-full max-h-80 h-full"
+          />
         </div>
       </MaxWidth>
     </div>
