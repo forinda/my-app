4f860ac51b7cdcec3f87e2e1453461450cf58ed5
Felix Orinda <forinda82@gmail.com>
2025-02-13 14:34:30 +0300

    Fix homepage render

--- packages/web/src/views/IndexPage.vue
+++ packages/web/src/views/IndexPage.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import HmpBanner from '@/components/HmpBanner.vue';
-import HmpWhyChoice from '@/components/HmpWhyChoice.vue';
-import HmpTeam from '@/components/HmpTeam.vue';
+import HmpBanner from '@/components/HmpBanner.vue'
+import HmpWhyChoice from '@/components/HmpWhyChoice.vue'
+import HmpTeam from '@/components/HmpTeam.vue'
+import { teamMembers } from '@/lib/data/team'
 </script>
 <template>
   <hmp-banner />
   <hmp-why-choice />
-  <hmp-team />
+  <hmp-team :members="teamMembers" />
 </template>
 
 <style scoped></style>
