e44d45d13f788faba51f1adabde3cde48131c3e3
Felix Orinda <forinda82@gmail.com>
2025-02-14 13:02:32 +0300

    Add build features using gulp

---
+++ tasks/build-apps.ts
@@ -0,0 +1,33 @@
+import { parallel } from 'gulp';
+import { exec } from 'shelljs';
+import { folders } from '../base-paths';
+
+function build_client_ui(cb: Function) {
+  const { BASE_DIR } = folders;
+  const command = `cd ${BASE_DIR} && pnpm --filter=web build`;
+  console.warn(`Running command: ${command}`);
+  exec(command);
+  cb();
+}
+
+function build_server(cb: Function) {
+  const { BASE_DIR } = folders;
+  const command = `cd ${BASE_DIR} && pnpm --filter=backend build`;
+  console.warn(`Running command: ${command}`);
+  exec(command);
+  cb();
+}
+
+function build_admin_client(cb: Function) {
+  const { BASE_DIR } = folders;
+  const command = `cd ${BASE_DIR} && pnpm --filter=admin build`;
+  console.warn(`Running command: ${command}`);
+  exec(command);
+  cb();
+}
+
+export const build_apps = parallel(
+  build_client_ui,
+  build_server,
+  build_admin_client,
+);
